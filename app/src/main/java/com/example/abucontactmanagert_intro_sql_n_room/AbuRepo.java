56344f7d85dd24f0cc48c3dbcd938a8ac11f5698
Abubakar Mohammed <ASMohammed@student.fullsail.edu>
2023-12-31 16:52:49 -0500

    example off handle,executor, & runnable all in one method

--- app/src/main/java/com/example/abucontactmanagert_intro_sql_n_room/AbuRepo.java
+++ app/src/main/java/com/example/abucontactmanagert_intro_sql_n_room/AbuRepo.java
@@ -1,6 +1,11 @@
 package com.example.abucontactmanagert_intro_sql_n_room;
 
+import android.os.Handler;
+import android.os.Looper;
+
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class AbuRepo {
 //if a method is in the DOA it has has to be in the repository
@@ -12,7 +17,23 @@ public class AbuRepo {
 
     public void AddContact(Contacts contacts)
     {
-        contactsAbuDoa.InsertAbu(contacts);
+        //Used backg database operation ensures order
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+        //Used for updating the UI
+        Handler handler = new Handler(Looper.getMainLooper()); // we used the os version of handler
+
+
+        //Runable: allows access to run method that allow the code to execute at the same time in the background
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                contactsAbuDoa.InsertAbu(contacts);
+
+            }
+        });
+
+        //contactsAbuDoa.InsertAbu(contacts);//we need to move the code above and add all those other codes to make sure this work properly
     }
 
     public void DeleteContact(Contacts contacts)
