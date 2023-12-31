57c42b372d5760d92cc788e3408dd7e32aaa8337
Abubakar Mohammed <ASMohammed@student.fullsail.edu>
2023-12-31 16:58:13 -0500

    example of moving the handler,executor & runnable outside the method so i can reuse it

--- app/src/main/java/com/example/abucontactmanagert_intro_sql_n_room/AbuRepo.java
+++ app/src/main/java/com/example/abucontactmanagert_intro_sql_n_room/AbuRepo.java
@@ -10,18 +10,20 @@ import java.util.concurrent.Executors;
 public class AbuRepo {
 //if a method is in the DOA it has has to be in the repository
     private final ContactsAbuDoa contactsAbuDoa;
-
+    ExecutorService executorService;
+    Handler handler;
     public AbuRepo(ContactsAbuDoa contactsAbuDoa) {
         this.contactsAbuDoa = contactsAbuDoa;
+        //Used backg database operation ensures order
+        executorService= Executors.newSingleThreadExecutor();
+
+        //Used for updating the UI
+        handler = new Handler(Looper.getMainLooper()); // we used the os version of handler
     }
 
     public void AddContact(Contacts contacts)
     {
-        //Used backg database operation ensures order
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
 
-        //Used for updating the UI
-        Handler handler = new Handler(Looper.getMainLooper()); // we used the os version of handler
 
 
         //Runable: allows access to run method that allow the code to execute at the same time in the background
@@ -38,7 +40,13 @@ public class AbuRepo {
 
     public void DeleteContact(Contacts contacts)
     {
-        contactsAbuDoa.DeleteAbu(contacts);
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                contactsAbuDoa.DeleteAbu(contacts);
+
+            }
+        });
     }
 
     public List<Contacts> GetAllContacts()
