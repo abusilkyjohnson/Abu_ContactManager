255e0d0479ba6175adb45e74c1dcb65339c6de05
Abubakar Mohammed <ASMohammed@student.fullsail.edu>
2023-12-31 21:48:15 -0500

    activity_main.xml added click handler also added

--- app/src/main/java/com/example/abucontactmanagert_intro_sql_n_room/AbuAdapter.java
+++ app/src/main/java/com/example/abucontactmanagert_intro_sql_n_room/AbuAdapter.java
@@ -1,4 +1,70 @@
 package com.example.abucontactmanagert_intro_sql_n_room;
 
-public class AbuAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.abucontactmanagert_intro_sql_n_room.databinding.ContactListLayoutBinding;
+
+import java.util.ArrayList;
+
+public class AbuAdapter extends RecyclerView.Adapter<AbuAdapter.ContactViewHolder> {
+
+    public AbuAdapter(ArrayList<Contacts> contacts) {
+        this.contacts = contacts;
+    }
+
+    private ArrayList<Contacts> contacts;
+
+    @NonNull
+    @Override
+    public ContactViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //creating new View items in recyclerView
+        ContactListLayoutBinding contactListLayoutBinding = DataBindingUtil.inflate(
+                LayoutInflater.from(parent.getContext()),
+                R.layout.contact_list_layout,parent,false);
+        return new ContactViewHolder(contactListLayoutBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ContactViewHolder holder, int position) {
+        //called by recyclerview when ir needs to display or update an item
+        //at a specific position in the list or grid
+
+        Contacts currentContact = contacts.get(position);
+        holder.contactListLayoutBinding.setContact(currentContact);
+    }
+
+    @Override
+    public int getItemCount() {
+        if(contacts != null)
+        {
+            return contacts.size();
+        }
+        else {
+            return 0;
+        }
+    }
+
+    public void setContacts(ArrayList<Contacts> contacts)
+    {
+        this.contacts = contacts;
+        //inform the recycler view that the underlying data set has changed and the recycler view should refresh its
+        //views to reflect these changes. Tells it the data has changed
+        notifyDataSetChanged();
+    }
+
+    class ContactViewHolder extends RecyclerView.ViewHolder {
+        private ContactListLayoutBinding contactListLayoutBinding;
+                                    //we removed the 1st parameter here
+        public ContactViewHolder(@NonNull ContactListLayoutBinding contactListLayoutBinding) {
+            super(contactListLayoutBinding.getRoot());//getroot provide access to the rootview allowing us to perform operation
+            this.contactListLayoutBinding = contactListLayoutBinding;
+        }
+    }
 }
--- app/src/main/java/com/example/abucontactmanagert_intro_sql_n_room/MainActivity.java
+++ app/src/main/java/com/example/abucontactmanagert_intro_sql_n_room/MainActivity.java
@@ -1,14 +1,74 @@
 package com.example.abucontactmanagert_intro_sql_n_room;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.abucontactmanagert_intro_sql_n_room.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Data Source
+    private ContactDataBase contactDatabase;
+    private ArrayList<Contacts> contacts = new ArrayList<>();
+
+    //Adapter
+    private AbuAdapter abuAdapter;
+
+    //binding
+    private ActivityMainBinding mainBinding;
+    private MainActivityClickHandler handlers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        handlers = new MainActivityClickHandler(this);
+
+        mainBinding.setAbuClickHandler(handlers);
+
+        //RecyclerView
+        RecyclerView recyclerView = mainBinding.recyclerView;//this line obatains a reference that we can interact with
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        //Adapter
+        abuAdapter = new AbuAdapter(contacts);
+
+        //Database
+        contactDatabase = ContactDataBase.GetInstance(this);
+
+        //ViewModel
+        ContactAppViewModel viewModel = new ViewModelProvider(this).get(ContactAppViewModel.class);
+
+        //Inserting a new contact for example reason
+        Contacts c1 = new Contacts(1,"Abu","Siklyjohnson@gmail.com");
+        viewModel.AddContact(c1);//we work directly with the viewmodel
+
+        //Loading the Data from ROOM db
+        viewModel.GetAllContact().observe(this, new Observer<List<Contacts>>() {
+            @Override
+            public void onChanged(List<Contacts> contacts) {
+                for (Contacts c: contacts)//for each loop
+                {
+                    Log.v("Tagy",c.get_name());
+                }
+            }
+        });
+
+        //Linking the RecyclerView with the Adapter
+        recyclerView.setAdapter(abuAdapter);
     }
 }
